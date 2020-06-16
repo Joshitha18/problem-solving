c0516c46b1174afd5d795f4bec9a08fca68d640d
joshithagandra <47497998+Joshitha18@users.noreply.github.com>
2020-06-16 10:53:04 +0530

    Update klargest.cpp

--- Leetcode/klargest.cpp
+++ Leetcode/klargest.cpp
@@ -1,39 +1,38 @@
-#include<iostream>
-using namespace std;
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+int s=nums.size();
+        
+int max= *max_element(nums.begin() ,nums.end())+1;
+        //cout<<max;
+int min= *min_element(nums.begin() ,nums.end())-1;
 
-int main(){
-int n,i,j,count=0,k,size,index;
-cin>>n;
-cin>>k;
-size=n;
-int a[1000],b[1000];
+ if(min<0){min=-min;}
+        
+vector <int> a(10000,0);
+vector <int> b(10000,0);
 
-for(i=0;i<1000;i++){a[i]=b[i]=0;}
+for(int i=0;i<s;i++){
+	if(nums[i]>=0){a[nums[i]]+=1;}
+	else if(nums[i]<0){nums[i]= -nums[i];b[nums[i]]+=1;}
+}
 
-while(n--){
-	cin>>j;
-	cout<<j<<endl;
-	if(j>=0){a[j]++;}
-	else if(j<0){j=-j;b[j]++;}
-	}
-n=size;	
-
-for(i=n;i>=0;i--){
+int count=0;
+   int i,index;     
+        
+for(i=max;i>=0;i--){
 count=count+a[i];
 if(count>=k) {index=i;break;}
 }
 
 i=1;
 
-
 while(count<k){
 count=count+b[i];
 if(count>=k) {index=-i;break;}
 i++;
-}
-
-cout<<index<<endl;
-
-return 0;
-}
+}      
+     return index;   
+    }
+};
 
