8c68e8ef16b2bb4bed9d063aa132dbe517a2f073
joshithagandra <47497998+Joshitha18@users.noreply.github.com>
2020-06-21 16:52:25 +0530

    Update Merge2LL.cpp

--- DataStructures/Linked List/Merge2LL.cpp
+++ DataStructures/Linked List/Merge2LL.cpp
@@ -1,1 +1,19 @@
-
+class Solution {
+public:
+    Node* mergeTwoLists(Node* list1, Node* list2) {
+        if(list1==NULL){
+            return list2;
+        }
+        if(list2==NULL){
+            return list1;
+        }
+        if(list1->data <= list2->data){
+            list1->next = mergeTwoLists(list1->next,list2);
+            return l1;
+        }
+        else{
+            list2->next = mergeTwoLists(list1,list2->next);
+            return list2;
+        }
+    }
+};
