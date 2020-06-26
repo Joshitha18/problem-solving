fb79132cdaa44c20def8373dd71c62f147accfff
joshithagandra <47497998+Joshitha18@users.noreply.github.com>
2020-06-26 09:04:49 +0530

    Create Count_Primes.cpp

---
+++ Leetcode/Count_Primes.cpp
@@ -0,0 +1,21 @@
+//Count the number of prime numbers less than a non-negative number, n.
+
+class Solution {
+public:
+    
+    int countPrimes(int n) {
+      int i,j,c=2;
+      vector <bool> visit(n,false);        
+        
+        for(i=2;i*i<n;i++){
+            if(visit[i]) continue;
+                for(j=i*i;j<n;j+=i){
+                  if(!visit[j] && j%i==0){visit[j]=true;c++;}
+                }            
+        }
+        
+        int count=n-c;
+        if (count<0) count=0;
+      return count;  
+    }
+};
