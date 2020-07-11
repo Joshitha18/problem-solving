661736d318fce4c7facd7a45582a63404cedf735
joshithagandra <joshitha1830@gmail.com>
2020-07-11 12:29:35 +0530

    Added #1

---
+++ DCP/001/sol.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    bool twoSum(vector<int>& nums, int target) {
+        unordered_map<int, bool> umap;
+        for(auto x:nums) {
+            if(umap.find(target-x)!=umap.end()) 
+                return true;
+            umap[x]=true;
+        }
+        return false;
+    }
+};
+
+int main(){
+  vector<int> nums{3,2,4};
+  int target = 6;
+  Solution sol;
+  cout << sol.twoSum(nums, target);
+}
+
