7c90930314fc443a2b7bb890653999d5f8976d99
Joshitha18 <joshitha1830@gmail.com>
2021-03-06 23:42:02 +0530

    leetcode

---
+++ platforms/Leetcode/DailyChallenge/March/5_Avg_of_levels_Binary_Tree/2.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<double> averageOfLevels(TreeNode* root) {
+        vector<double> ans;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty())
+        {
+            double sum=0;
+            int ts=q.size();
+            for(int i=0;i<ts;i++)
+            {
+                sum+=q.front()->val;
+                if(q.front()->left)
+                {
+                    q.push(q.front()->left);
+                }
+                if(q.front()->right)
+                {
+                    q.push(q.front()->right);
+                }
+                q.pop();
+            }
+            ans.push_back(sum/ts);
+        }
+        return ans;
+    }
+};
